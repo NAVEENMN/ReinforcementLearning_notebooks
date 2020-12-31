10642f9e790bf0eb39b551733753e26c994c8613
naveenmysore <navimn1991@gmail.com>
2020-12-31 14:19:28 -0800

    adding one more eps

--- epsilon_greedy/main.py
+++ epsilon_greedy/main.py
@@ -1,7 +1,12 @@
 import numpy as np
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
+import multiprocessing
 
+K = 10
+EPISODES = 1000
+TIME_STEPS = 1000
 
 class RewardDistribution:
     def __init__(self, k=10):
@@ -92,33 +97,80 @@ class EpsBandit:
 
 
 def run_experiment():
-    k = 3
-    rd = RewardDistribution(k=k)
-
+    rd = RewardDistribution(k=K)
     data = {}
 
-    eps_0 = EpsBandit(rd=rd, k=k, eps=0.0, iterations=1000)
+    eps_0 = EpsBandit(rd=rd, k=K, eps=0.0, iterations=TIME_STEPS)
     data['eps_0'] = eps_0.run()
 
-    eps_0_0_1 = EpsBandit(rd=rd, k=k, eps=0.01, iterations=1000)
+    eps_0_0_1 = EpsBandit(rd=rd, k=K, eps=0.01, iterations=TIME_STEPS)
     data['eps_0_0_1'] = eps_0_0_1.run()
 
-    eps_0_1 = EpsBandit(rd=rd, k=k, eps=0.1, iterations=1000)
+    eps_0_1 = EpsBandit(rd=rd, k=K, eps=0.1, iterations=TIME_STEPS)
     data['eps_0_1'] = eps_0_1.run()
+
+    eps_0_5 = EpsBandit(rd=rd, k=K, eps=0.5, iterations=TIME_STEPS)
+    data['eps_0_5'] = eps_0_5.run()
 
     return data
 
+def prepare_data_for_plotting(_df):
+    entries = []
+    for time_step in range(0, TIME_STEPS):
+        entries.append({'time_step': time_step, 'epsilon': 'eps_0', 'average_reward': _df['eps_0'][time_step]})
+        entries.append({'time_step': time_step, 'epsilon': 'eps_0_0_1', 'average_reward': _df['eps_0_0_1'][time_step]})
+        entries.append({'time_step': time_step, 'epsilon': 'eps_0_1', 'average_reward': _df['eps_0_1'][time_step]})
+        entries.append({'time_step': time_step, 'epsilon': 'eps_0_5', 'average_reward': _df['eps_0_5'][time_step]})
+    dframe = pd.DataFrame(entries)
+    return dframe
+
 
 def run_episodes():
-    episodes = 100
     result = dict()
-    result['eps_0'] = np.zeros(1000)
-    result['eps_0_0_1'] = np.zeros(1000)
-    result['eps_0_1'] = np.zeros(1000)
-    for episode in range(1, episodes):
+    result['eps_0'] = np.zeros(TIME_STEPS)
+    result['eps_0_0_1'] = np.zeros(TIME_STEPS)
+    result['eps_0_1'] = np.zeros(TIME_STEPS)
+    result['eps_0_5'] = np.zeros(TIME_STEPS)
+    for episode in range(1, EPISODES):
         df = pd.DataFrame(run_experiment())
         result['eps_0'] = running_average(m_n_1=result['eps_0'], r_i=np.asarray(df['eps_0']), n=episode)
         result['eps_0_0_1'] = running_average(m_n_1=result['eps_0_0_1'], r_i=np.asarray(df['eps_0_0_1']), n=episode)
         result['eps_0_1'] = running_average(m_n_1=result['eps_0_1'], r_i=np.asarray(df['eps_0_1']), n=episode)
+        result['eps_0_5'] = running_average(m_n_1=result['eps_0_5'], r_i=np.asarray(df['eps_0_5']), n=episode)
         _df = pd.DataFrame(result)
-    return _df
+        dframe = prepare_data_for_plotting(_df)
+        plt.figure()
+        pl = sns.lineplot(data=dframe, x='time_step', y='average_reward', hue='epsilon')
+        pl.set_ylim(-1.5, 2)
+        pl.set_title(f'epsilon greedy approach - episode {episode}')
+        plt.legend(loc='lower right')
+        plt.savefig(f"/Users/naveenmysore/Documents/plots/episode_{episode}.png")
+        plt.clf()
+    return dframe
+
+def create_gif():
+    import os
+    import glob
+    from PIL import Image
+    # filepaths
+    fp_in = "/Users/naveenmysore/Documents/plots/episode_*.png"
+    fp_out = "/Users/naveenmysore/Documents/plots/epsilon_greedy.gif"
+
+    # https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html#gif
+    img, *imgs = [Image.open(f"/Users/naveenmysore/Documents/plots/episode_{x}.png") for x in range(1, EPISODES)]
+    img.save(fp=fp_out, format='GIF', append_images=imgs,
+             save_all=True, duration=10, loop=0)
+
+    # delete all png files.
+    for f in glob.glob(fp_in):
+        os.remove(f)
+
+
+_df = run_episodes()
+create_gif()
+
+# save final plot
+snplt = sns.lineplot(data=_df, x='time_step', y='average_reward', hue='epsilon')
+snplt.figure.savefig("/Users/naveenmysore/Documents/plots/output.png")
+
+print("plots are saved to /Users/naveenmysore/Documents/plots/")
